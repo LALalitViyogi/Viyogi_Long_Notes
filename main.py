587946c6fb7fc1a5bfab7dc49fb71e4d5d2687bf
L.A. Lalit Viyogi <viyogi.lalit786@gmail.com>
2021-01-07 15:04:33 +0530

    ALL TOOLBAR FUNCTIONALITY AND SHORTCUT KEY

--- main.py
+++ main.py
@@ -11,9 +11,7 @@ main_application.title("Viyogi Long Notes")
 main_application.wm_iconbitmap('icon.ico')
 
 ################################################# main menu front end ###################################
-
 main_menu= tk.Menu()
-
 
 ###### File menu ########
 # //////Using icons///// right now its code is comment out.
@@ -25,7 +23,6 @@ exit_icon=tk.PhotoImage(file="icons2/exit.png")
 
 files = tk.Menu(main_menu,tearoff=False)
 
-
 ###### Edit menu ########
 # //////Using icons///// right now its code is comment out.
 copy_icon=tk.PhotoImage(file="icons2/copy.png")
@@ -35,7 +32,6 @@ clear_all_icon=tk.PhotoImage(file="icons2/clear_all.png")
 find_icon=tk.PhotoImage(file="icons2/find.png")
 
 edit = tk.Menu(main_menu,tearoff=False)
-
 
 ###### View menu ########
 
@@ -80,7 +76,6 @@ main_menu.add_cascade(label='Speak it!', menu=speaked)
 
 
 #----------------------------&&&&&&&------ end main menu front end ------&&&&&----------------------#
-
 
 ###################################################### toolbar #########################################
 
@@ -144,7 +139,6 @@ speak_btn.grid(row=0,column=9, padx=5)
 
 #------------------------------&&&&&&&&&&-- end tool bar ----&&&&&&&&&&&-------------------------------#
 
-
 ################################################ text edit space ####################################
 text_editor=tk.Text(main_application)
 text_editor.config(wrap='word',relief=tk.FLAT)
@@ -166,13 +160,10 @@ def change_font_family(main_application):
     curr_font_family = font_family.get()
     text_editor.configure(font=(curr_font_family,curr_font_size,))
 
-
-
 def change_font_size(main_application):
     global curr_font_size
     curr_font_size=size_var.get()
     text_editor.configure(font=(curr_font_family,curr_font_size))
-
 font_box.bind("<<ComboboxSelected>>", change_font_family)
 font_size.bind("<<ComboboxSelected>>", change_font_size)
 ## --------------- basic font functionality done -----------------###
@@ -222,7 +213,6 @@ def change_to_left():
     text_editor.tag_config('left', justify=tk.LEFT)
     text_editor.delete(1.0,tk.END)
     text_editor.insert(tk.INSERT,text_content,'left')
-
 align_left_btn.configure(command=change_to_left)
 
 
@@ -243,7 +233,7 @@ def change_to_right():
     text_editor.insert(tk.INSERT,text_content,'right')
 align_right_btn.configure(command=change_to_right)
 
-text_editor.configure(font=('Arial',24))
+text_editor.configure(font=('Arial',12))
 
 #---------------------------&&&&&&&&&&&&---------- end text edit space -----------&&&&&&&&&&&&--------------#
 
@@ -259,6 +249,11 @@ def changed(event=None):
         words = len(text_editor.get(1.0, 'end-1c').split())
         characters = len(text_editor.get(1.0, 'end-1c'))
         status_bar.config(text=f'Characters : {characters} Words : {words}')
+    
+    # this is for adding '*' in the name of file in application title
+    #to show that the FILE IS UN-SAVED.
+    if text_editor.edit_modified() and file_url:
+        main_application.title('*'+str(os.path.basename(file_url)))
     text_editor.edit_modified(False)
 
 text_editor.bind('<<Modified>>', changed)
@@ -276,7 +271,6 @@ def new_file(event=None):
         file_url=""
     text_editor.delete(1.0,tk.END)
     main_application.title("Untitled file")
-
 
 ## adding commands in file menu.
 files.add_command(label="New",compound=tk.LEFT,image=new_icon, accelerator="CTRL+N", command=new_file)
@@ -299,7 +293,6 @@ def open_file(event=None):
 files.add_command(label="Open",compound=tk.LEFT, image=open_icon ,accelerator="CTRL+O", command=open_file)
 
 
-
 ### SAVE FILE FUNCTIONALITY _________________________
 
 def save_file(event=None):
@@ -314,6 +307,8 @@ def save_file(event=None):
             content2=text_editor.get(1.0,tk.END)
             file_url.write(content2)
             file_url.close()
+        # this is remove '*' from the title name of file in application title
+        main_application.title(os.path.basename(file_url))  
     except:
         return
 
@@ -329,6 +324,7 @@ def save_as_file(event=None):
         file_url.close()
     except :
         return
+
 files.add_command(label="Save as",compound=tk.LEFT, image=save_as_icon ,accelerator="CTRL+SHIFT+S", command=save_as_file)
 
 ### Exit Functionality ---------
@@ -359,42 +355,156 @@ def exit_func(event=None):
     except :
         return
 
-
 files.add_command(label="Exit",compound=tk.LEFT, image=exit_icon ,accelerator="CTRL+Q",command=exit_func)
 
 
-
 #/////////// adding commands in  edit menu.
-edit.add_command(label="Copy",compound=tk.LEFT,image=copy_icon ,accelerator="CTRL+C")
-edit.add_command(label="Cut",compound=tk.LEFT,image=cut_icon ,accelerator="CTRL+X")
-edit.add_command(label="Paste",compound=tk.LEFT,image=paste_icon ,accelerator="CTRL+V")
-edit.add_command(label="Clear all",compound=tk.LEFT,image=clear_all_icon ,accelerator="CTRL+ALT+X")
-edit.add_command(label="Find",compound=tk.LEFT,image=find_icon ,accelerator="CTRL+F")
+edit.add_command(label="Copy",compound=tk.LEFT,image=copy_icon ,accelerator="CTRL+C",command=lambda:text_editor.event_generate("<Control c>") )
+edit.add_command(label="Cut",compound=tk.LEFT,image=cut_icon ,accelerator="CTRL+X", command=lambda:text_editor.event_generate("<Control x>"))
+edit.add_command(label="Paste",compound=tk.LEFT,image=paste_icon ,accelerator="CTRL+V", command=lambda:text_editor.event_generate("<Control v>"))
+edit.add_command(label="Clear all",compound=tk.LEFT,image=clear_all_icon ,accelerator="CTRL+ALT+X", command=lambda:text_editor.delete(1.0,tk.END))
+def find_func(event=None):
+    find_box=tk.Toplevel()
+    find_box.geometry('450x250+500+200')
+    find_box.title('Find & Replace')
+    find_box.resizable(0,0)
+    ##functions
+    def findnow():
+        word=find_entry.get()
+        number_matches=0
+        data=str(text_editor.get(1.0,tk.END))
+        text_editor.tag_remove('match', '1.0', tk.END)
+        if word:
+            start='1.0'
+            while True:
+                start = text_editor.search(word, start, stopindex=tk.END)
+                if not start:
+                    break 
+                end_pos = f'{start}+{len(word)}c'
+                text_editor.tag_add('match', start, end_pos)
+                number_matches += 1
+                start =end_pos
+                text_editor.tag_config('match', foreground='yellow', background='red')
+                
+               
+    def replacenow():
+        word = find_entry.get()
+        replace_text = replace_entry.get()
+        content = text_editor.get(1.0, tk.END)
+        new_content = content.replace(word, replace_text)
+        text_editor.delete(1.0, tk.END)
+        text_editor.insert(1.0, new_content)
+    #visualcreations
+    #frame
+    find_frames=ttk.LabelFrame(find_box,text='FIND/REPLACE')
+    find_frames.pack(pady=20)
 
+    #labels
+    find_label=ttk.Label(find_frames,text='Find :')
+    replace_label=ttk.Label(find_frames,text='Replace :')
+
+    #entry box
+    find_entry=ttk.Entry(find_frames,width=30)
+    replace_entry=ttk.Entry(find_frames,width=30)
+
+    #button
+    find_button=ttk.Button(find_frames,text='Find',command=findnow)
+    replace_button=ttk.Button(find_frames,text='Replace',command=replacenow)
+
+    ####packing/griding
+    #grid_labels
+    find_label.grid(row=0,column=0,padx=4,pady=4)
+    replace_label.grid(row=1,column=0,padx=4,pady=4)
+
+    #grid_entrybox
+    find_entry.grid(row=0,column=1,padx=4,pady=4)
+    replace_entry.grid(row=1,column=1,padx=4,pady=4)
+
+    #grid_button
+    find_button.grid(row=2,column=0,padx=8,pady=4)
+    replace_button.grid(row=2,column=1,padx=8,pady=4)
+
+    find_box.mainloop()
+
+edit.add_command(label="Find",compound=tk.LEFT,image=find_icon ,accelerator="CTRL+F", command=find_func)
 
 
 #////////////// adding command in view menu.
-view.add_checkbutton(label='ToolBar',image=tool_bar_icon, compound=tk.LEFT)
-view.add_checkbutton(label='StatusBar',image=status_bar_icon, compound=tk.LEFT)
+show_statusbar = tk.BooleanVar()
+show_statusbar.set(True)
+show_toolbar = tk.BooleanVar()
+show_toolbar.set(True)
+
+def hide_toolbar():
+    global show_toolbar
+    if show_toolbar:
+        tool_bar.pack_forget()
+        show_toolbar = False 
+    else :
+        text_editor.pack_forget()
+        status_bar.pack_forget()
+        tool_bar.pack(side=tk.TOP, fill=tk.X)
+        text_editor.pack(fill=tk.BOTH, expand=True)
+        status_bar.pack(side=tk.BOTTOM)
+        show_toolbar = True
+
+def hide_statusbar():
+    global show_statusbar
+    if show_statusbar:
+        status_bar.pack_forget()
+        show_statusbar = False
+    else :
+        status_bar.pack(side=tk.BOTTOM)
+        show_statusbar = True
+
+view.add_checkbutton(label='ToolBar',image=tool_bar_icon, onvalue=True, offvalue=0 , variable = show_toolbar , compound=tk.LEFT, command=hide_toolbar)
+view.add_checkbutton(label='StatusBar',image=status_bar_icon,onvalue=1, offvalue=False,variable = show_statusbar ,compound=tk.LEFT,command=hide_statusbar)
 
 #/////////////////adding command in color theme.
+def change_theme():
+    choosen_theme=theme_choice.get()
+    color_tuple=color_dict.get(choosen_theme)
+    fg_color , bg_color = color_tuple[0],color_tuple[1]
+    text_editor.config(background=bg_color,fg=fg_color)
 count = 0 
 for i in color_dict:
-    color_theme.add_radiobutton(label = i, image=color_icons[count], variable=theme_choice, compound=tk.LEFT)
+    color_theme.add_radiobutton(label = i, image=color_icons[count], variable=theme_choice, compound=tk.LEFT,command=change_theme)
     count += 1
-
 
 #//////////////// Speak Command.
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-def Speaktext(audio):
- engine.say(audio)
- engine.runAndWait()
+
+def Speaktext():
+    audio=str(text_editor.get(1.0,tk.END))
+    engine.say(audio)
+    engine.runAndWait()
+
 speaked.add_command(label='Speak',compound=tk.LEFT,command=Speaktext,accelerator='CTRL+T')
 speak_btn.config(command=Speaktext)
 
-
 #---------------------------- end main menu back end --------------------#
+
+### window protocol event handelling
+main_application.protocol("WM_DELETE_WINDOW",exit_func)
+
 main_application.config(menu=main_menu)
+
+## Binding Keys
+
+main_application.bind("<Control-n>", new_file)
+main_application.bind("<Control-o>", open_file)
+main_application.bind("<Control-s>", save_file)
+main_application.bind("<Control-Alt-s>", save_as_file)
+main_application.bind("<Control-q>", exit_func)
+main_application.bind("<Control-f>", find_func)
+## for enabling binding keys when Capslock is on.
+main_application.bind("<Control-N>", new_file)
+main_application.bind("<Control-O>", open_file)
+main_application.bind("<Control-S>", save_file)
+main_application.bind("<Control-Alt-S>", save_as_file)
+main_application.bind("<Control-Q>", exit_func)
+main_application.bind("<Control-F>", find_func)
+
 main_application.mainloop()
